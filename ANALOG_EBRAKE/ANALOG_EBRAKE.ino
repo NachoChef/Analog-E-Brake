8a5667653289746d8ae174c3339ccfcfef2a98b8
Justin Jones <hock3ydud3@gmail.com>
2017-10-04 14:42:51 -0500

    Update ANALOG_EBRAKE.ino

--- ANALOG_EBRAKE/ANALOG_EBRAKE.ino
+++ ANALOG_EBRAKE/ANALOG_EBRAKE.ino
@@ -5,17 +5,46 @@
 // by AMSTUDIO
 // 20.1.2017
 
+
+// Adding additional support for more buttons. More buttons = more juice.
+// Additional button code based off the `Arduino - Switch` example.
+
+// todo - Wire buttons, create array, get more juice.
 #include <Joystick.h>
 
+const int eBrakePin = A0;
+const int btn1 = 2;
+int btn1_state = HIGH;
+int btn1_read;
+int previous = LOW;
+long time = 0;
+long debounce = 200;
+
 void setup()
-
-{pinMode(A0, INPUT); 
-   Joystick.begin();}
-
-const int pinToButtonMap = A0;
+{
+   pinMode(eBrakePin, INPUT); 
+   pinMode(btn1, PULL_UP);
+   Joystick.begin();
+}
 
 void loop()
+{
+   // digital logic
+   btn1_read = digitalRead(btn1);
+   if (btn1_read == HIGH && previous == LOW && millis() - time > debounce) {
+       if (state == HIGH)
+         state = LOW;
+       else
+         state = HIGH;
 
-{int pot = analogRead(A0);
-int mapped = map(pot,0,1023,0,255);
-{Joystick.setThrottle(mapped);}}
+       time = millis();   
+       Joystick.sendState(btn1_state);
+   }
+   
+   // analog logic
+   int pot = analogRead(A0);
+   int mapped = map(pot,0,1023,0,255);
+   {
+      Joystick.setThrottle(mapped);
+   }
+}
